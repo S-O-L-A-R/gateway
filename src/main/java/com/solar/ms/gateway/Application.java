0f71fb3502f8819b37b49fc61d538100dc8f01fb
Pongsakorn Somsri <pongsakorns@gmail.com>
2019-09-11 02:57:39 +0700

    Test config

--- src/main/java/com/solar/ms/gateway/Application.java
+++ src/main/java/com/solar/ms/gateway/Application.java
@@ -2,7 +2,9 @@ package com.solar.ms.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
---
+++ src/main/java/com/solar/ms/gateway/controller/GatewayController.java
@@ -0,0 +1,19 @@
+package com.solar.ms.gateway.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class GatewayController {
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/services")
+    public List<String> services() {
+        return this.discoveryClient.getServices();
+    }
+}
